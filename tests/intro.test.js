c6a9d834faebf54df6980181a2f13f0b42426ca1
Anand Mainali <anand.mainali@cyberarrow.io>
2024-08-06 15:29:11 +0545

    test-driven-development-approach

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,4 +1,4 @@
-import { fizzBuzz, max } from '../src/intro'
+import { calculateAverage, fizzBuzz, max } from '../src/intro'
 import {describe,it,expect} from 'vitest'
 
 // describe('max',()=>{
@@ -13,17 +13,29 @@ import {describe,it,expect} from 'vitest'
 //     })
 // })
 
-describe('fizzbuzz',()=>{
-    it('should return FizzBuzz if n is divided by both 3 and 5',()=>{
-        expect(fizzBuzz(15)).toBe('FizzBuzz')
+// describe('fizzbuzz',()=>{
+//     it('should return FizzBuzz if n is divided by both 3 and 5',()=>{
+//         expect(fizzBuzz(15)).toBe('FizzBuzz')
+//     })
+//     it('should return Fizz if n is divided by 3',()=>{
+//         expect(fizzBuzz(12)).toBe('Fizz')
+//     })
+//     it('should return FizzBuzz if n is divided by 5',()=>{
+//         expect(fizzBuzz(25)).toBe('Buzz')
+//     })
+//     it('should return string if not divided by both 3 and 5',()=>{
+//         expect(fizzBuzz(22)).toBe('22')
+//     })
+// })
+
+describe('calculateAverage',()=>{
+    it('should return NaN if there is empty array',()=>{
+        expect(calculateAverage([])).toBe(NaN)
     })
-    it('should return Fizz if n is divided by 3',()=>{
-        expect(fizzBuzz(12)).toBe('Fizz')
+    it('should return element if there is only one element',()=>{
+        expect(calculateAverage([2])).toBe(2)
     })
-    it('should return FizzBuzz if n is divided by 5',()=>{
-        expect(fizzBuzz(25)).toBe('Buzz')
-    })
-    it('should return string if not divided by both 3 and 5',()=>{
-        expect(fizzBuzz(22)).toBe('22')
+    it('should return avg if there is more than one element',()=>{
+        expect(calculateAverage([1,2])).toBe(1.5)
     })
 })
