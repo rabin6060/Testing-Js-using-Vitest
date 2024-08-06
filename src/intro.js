1e7d3328f0fa3ed1e870a71e2cc117ada869b5aa
Anand Mainali <anand.mainali@cyberarrow.io>
2024-08-06 15:30:26 +0545

    write functionality based on test cases

--- src/intro.js
+++ src/intro.js
@@ -11,3 +11,16 @@ export function fizzBuzz(n) {
 
   return n.toString();
 }
+//tdd (test -driven - development)
+//in this we first write test and only implement production code .
+//first we write failed case and write a production code to pass cases
+export function calculateAverage(array){
+  if(array.length===0) return NaN
+  if (array.length===1) {
+    return array[0]
+  }
+   const sum = array.reduce((total,current)=>
+      total+current
+    ,0)
+   return sum/array.length
+}
